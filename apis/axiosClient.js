1ba663d6f486b1c72d25fbe44b263253b6d95f48
Pranav R R <pranav.rk02@gmail.com>
2023-03-18 11:13:12 +0530

    New Packages added

--- apis/axiosClient.js
+++ apis/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: `http://192.168.38.244:3000/api/v1`,
+  baseURL: `http://192.168.70.244:3000/api/v1`,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
