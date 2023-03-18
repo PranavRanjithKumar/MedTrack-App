3e4847f89367ab53f29ac51f6a18ee6a348a4fcf
Pranav R R <pranav.rk02@gmail.com>
2023-03-18 11:08:47 +0530

    Make List more generic

--- components/ItemsList.js
+++ components/ItemsList.js
@@ -1,18 +1,28 @@
 import { FlatList } from 'react-native';
 import React from 'react';
 import OrganizationItem from './OrganizationItem';
+import CatalogueItem from './CatalogueItem';
+import CartItem from './CartItem';
 
-const simpleRenderer = (item) => {
-  return <OrganizationItem {...item.item} />;
-};
+const ItemsList = ({ id, data, component, ...props }) => {
+  const simpleRenderer = (item) => {
+    if (component === 'organization') {
+      return <OrganizationItem {...item.item} />;
+    }
+    if (component === 'catalogue') {
+      return <CatalogueItem {...item.item} {...props} />;
+    }
+    // else if (component === 'catalogue') {
+    return <CartItem {...item.item} />;
+    // }
+  };
 
-const ItemsList = ({ data }) => {
   return (
     <FlatList
       style={{ paddingHorizontal: 22 }}
       data={data}
       renderItem={simpleRenderer}
-      keyExtractor={(item) => item._id}
+      keyExtractor={(item) => item[id]}
     />
   );
 };
