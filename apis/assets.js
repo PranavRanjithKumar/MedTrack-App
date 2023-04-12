152416fee096ec72efa79a90e28b40e7e9129e83
Pranav R R <pranav.rk02@gmail.com>
2023-04-13 01:03:42 +0530

    Build apis

---
+++ apis/assets.js
@@ -0,0 +1,115 @@
+import axiosClient from './axiosClient';
+
+export const getAssetDetails = async ({ orgId, assetId }) => {
+  try {
+    const response = await axiosClient.get(
+      `/organizations/${orgId}/assets/${assetId}`
+    );
+    return response.data;
+  } catch (e) {
+    throw new Error(e.response.data.message);
+  }
+};
+
+export const getOutSourcedAssets = async (id) => {
+  try {
+    const response = await axiosClient.get(
+      `/organizations/${id}/assets/out-sourced`
+    );
+    return response.data;
+  } catch (e) {
+    throw new Error(e.response.data.message);
+  }
+};
+
+export const getInHouseAssets = async (id) => {
+  try {
+    const response = await axiosClient.get(
+      `/organizations/${id}/assets/in-house`
+    );
+    return response.data;
+  } catch (e) {
+    throw new Error(e.response.data.message);
+  }
+};
+
+export const getInHouseAssetsForCatalogueId = async (id, catalogueId) => {
+  try {
+    const response = await axiosClient.get(
+      `/organizations/${id}/assets/in-house?catalogueId=${catalogueId}`
+    );
+    return response.data;
+  } catch (e) {
+    throw new Error(e.response.data.message);
+  }
+};
+
+export const storeSupplierAsset = async ({
+  id,
+  code,
+  manufacturingDate,
+  expiryDate,
+  latitude,
+  longitude,
+  quantity,
+}) => {
+  try {
+    const response = await axiosClient.post(
+      `/organizations/${id}/assets/`,
+      JSON.stringify({
+        code,
+        manufacturingDate,
+        expiryDate,
+        latitude,
+        longitude,
+        quantity,
+      })
+    );
+    return response.data;
+  } catch (e) {
+    throw new Error(e.response.data.message);
+  }
+};
+
+export const storeManufcturerAsset = async ({
+  id,
+  code,
+  manufacturingDate,
+  expiryDate,
+  latitude,
+  longitude,
+  quantity,
+  constitution,
+}) => {
+  try {
+    const response = await axiosClient.post(
+      `/organizations/${id}/assets/`,
+      JSON.stringify({
+        code,
+        manufacturingDate,
+        expiryDate,
+        latitude,
+        longitude,
+        quantity,
+        constitution,
+      })
+    );
+    return response.data;
+  } catch (e) {
+    throw new Error(e.response.data.message);
+  }
+};
+
+// export const fetchAssetDetails
+
+export const fetchAssetProvenance = async ({ orgId, assetId }) => {
+  try {
+    const response = await axiosClient.post(
+      `/organizations/${orgId}/provenance`,
+      JSON.stringify({ id: assetId })
+    );
+    return response.data;
+  } catch (e) {
+    throw new Error(e.response.data.message);
+  }
+};
--- apis/requests.js
+++ apis/requests.js
@@ -9,6 +9,47 @@ export const getRequestableOrganizations = async () => {
   }
 };
 
+export const getAllTransfers = async (id) => {
+  try {
+    const response = await axiosClient.get(`/organizations/${id}/transfers`);
+    return response.data;
+  } catch (e) {
+    throw new Error(e.response.data.message);
+  }
+};
+
+export const getAllRequests = async (id) => {
+  try {
+    const response = await axiosClient.get(`/organizations/${id}/demands`);
+    return response.data;
+  } catch (e) {
+    throw new Error(e.response.data.message);
+  }
+};
+
+export const placeRequest = async ({
+  id,
+  transferringOrgId,
+  latitude,
+  longitude,
+  requestedItems,
+}) => {
+  try {
+    const response = await axiosClient.post(
+      `/organizations/${id}/demands/`,
+      JSON.stringify({
+        transferringOrgId,
+        latitude,
+        longitude,
+        requestedItems,
+      })
+    );
+    return response.data;
+  } catch (e) {
+    throw new Error(e.response.data.message);
+  }
+};
+
 export const getCatalogueForOrganization = async (id) => {
   try {
     const response = await axiosClient.get(`/organizations/${id}/catalogue`);
@@ -22,6 +63,29 @@ export const getUnfulfilledTransfers = async (id) => {
   try {
     const response = await axiosClient.get(
       `/organizations/${id}/transfers/unfulfilled`
+    );
+    return response.data;
+  } catch (e) {
+    throw new Error(e.response.data.message);
+  }
+};
+
+export const makeTransfer = async ({
+  id,
+  requestId,
+  latitude,
+  longitude,
+  sentItems,
+}) => {
+  try {
+    const response = await axiosClient.post(
+      `/organizations/${id}/transfers/`,
+      JSON.stringify({
+        requestId,
+        latitude,
+        longitude,
+        sentItems,
+      })
     );
     return response.data;
   } catch (e) {
